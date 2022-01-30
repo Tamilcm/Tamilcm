8b616a929646b9605641f4c6212fa3b300cdadbe
Tamilcm <chinnamani0007028@gmail.com>
2022-01-31 01:14:09 +0530

    My task

---
+++ src/family/amma.java
@@ -0,0 +1,16 @@
+package family;
+
+public class amma {
+	public void parent() {
+		System.out.println("she only hold mother ");
+	}
+
+	public static void main(String[] args) {
+	amma n=new amma();
+	n.parent();
+appa s=new appa();
+s.children();
+
+	}
+
+}
---
+++ src/family/appa.java
@@ -0,0 +1,16 @@
+package family;
+
+public class appa {
+
+	public void children() {
+		System.out.println("they have two childrens");
+
+	}
+	public static void main(String[] args) {
+		appa m = new appa();
+		m.children();
+
+
+	}
+
+}
